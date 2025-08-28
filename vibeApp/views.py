b5b5fc882f66380cdeb088823d24b3120ef5d1e2
amal_jith <123450863+amal-jith@users.noreply.github.com>
2025-08-28 17:05:30 +0530

    Update views.py

--- vibeApp/views.py
+++ vibeApp/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 def home(request):
 
     return render(request, "home.html", )
+
+def corporate(request):
+    return render(request, "corporate.html")
+
+
+def socialEvents(request):
+    return render(request, "social-events.html")
+
+
+def weddings(request):
+    return render(request, "weddings.html")
+
+def about(request):
+    return render(request, "about.html")
+
+
+def gallery(request):
+    return render(request, "gallery.html")
