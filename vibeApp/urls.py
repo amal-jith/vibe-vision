52a0dc2ae06fa836bf4c2716cd891834ecac2d6d
amal_jith <123450863+amal-jith@users.noreply.github.com>
2025-08-28 17:05:07 +0530

    Update urls.py

--- vibeApp/urls.py
+++ vibeApp/urls.py
@@ -7,4 +7,9 @@ app_name = "vibeApp"
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('corporate-events', views.corporate, name='corporate'),
+    path('social-events', views.socialEvents, name='social'),
+    path('weddings', views.weddings, name='weddings'),
+    path('about-us', views.about, name='about'),
+    path('gallery', views.gallery, name='gallery'),
 ]
